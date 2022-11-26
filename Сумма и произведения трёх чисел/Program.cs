f169be191cd5c451916ca4984b29d2132149bef3
Sobirjon Matnazarov <matnazarovsobirjon123@outlook.com>
2022-11-26 23:27:50 +0500

    Исправления в коде

--- Сумма и произведения трёх чисел/Program.cs
+++ Сумма и произведения трёх чисел/Program.cs
@@ -4,15 +4,20 @@
     {
         static void Main(string[] args)
         {
-            int a = 4;
-            int b = 5;
-            int c = 6;
+            //Задание такое: Нужно считать с пользователья три числа, потом сложить их, и произвести.
+            //Так вот, Я сделал значения зашитый в коде, и не понимая термин Произведения, сделал вычетания.
+            //Но потом исправил ситуацию, скопировал немного кода у автора курса :)
+            
+            Console.WriteLine("Введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье число:");
+            int c = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Сумма и произведения трёх чисел");
-            Console.WriteLine("===============================");
             Console.WriteLine($"Заданы: a = {a}, b = {b}, c = {c}");
             Console.WriteLine("Сумма => " + (a + b + c));
-            Console.WriteLine("Произведения => " + (a - b - c));
+            Console.WriteLine("Произведения => " + (a * b * c));
         }
     }
 }
