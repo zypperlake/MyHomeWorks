ae172d13dda61788d86462c4f9779cd0dcb01aa0
Sobirjon Matnazarov <matnazarovsobirjon123@outlook.com>
2022-11-26 23:19:19 +0500

    Исправил домашнее задание.

--- Среднее арифметическое двух чисел/Program.cs
+++ Среднее арифметическое двух чисел/Program.cs
@@ -4,33 +4,19 @@
     {
         static void Main(string[] args)
         {
-            bool debug = false;
-            
-            Console.WriteLine("Среднее арифметическое двух чисел");
-            Console.WriteLine("=================================");
+            //Задание такое: Нужно вывести среднее арифметическое двух чисел, 
+            //числа мы принимаем от пользователья. Что я не сделал жо этого, это не предусмотрел
+            //вариант ввода дробных чисел, что потом исправил :)
+
+            double firstValue, secondValue;
 
             Console.Write("Введите первое число => ");
-            byte num1 = Convert.ToByte(Console.ReadLine());
-            if (debug) { Console.WriteLine("\n" + Environment.StackTrace + "\n"); }
-
+            firstValue = double.Parse(Console.ReadLine());
+            Console.Write("\n");
             Console.Write("Введите второе число => ");
-            int num2 = Convert.ToByte(Console.ReadLine());
-            if (debug) { Console.WriteLine("\n" + Environment.StackTrace + "\n"); }
-
-            Console.WriteLine("=================================");
-            Console.WriteLine($"Результат операции   => {(num1 + num2) / 2}");
-            if (debug) { Console.WriteLine("\n" + Environment.StackTrace + "\n"); }
-
-            Console.WriteLine("=================================");
-            Console.WriteLine("\n" + "Дополнительная информация:");
-            string format = "      {0,3}\t|    {1,4} ";
-            Console.WriteLine("=================================");
-            Console.WriteLine(string.Format(format, "Тип", "Значение"));
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine(string.Format(format, num1.GetTypeCode(), num1));
-            Console.WriteLine(string.Format(format, num2.GetTypeCode(), num2));
-            Console.WriteLine("=================================");
-            if (debug) { Console.WriteLine("\n" + Environment.StackTrace + "\n"); }
+            secondValue = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write("Результат операции: " + (firstValue + secondValue) / 2);
         }
     }
 }
