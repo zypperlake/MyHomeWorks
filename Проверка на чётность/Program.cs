1d0390d2b0e71f4ce0df493cce77f055bcb7a256
Sobirjon Matnazarov <matnazarovsobirjon123@outlook.com>
2022-11-27 01:50:02 +0500

    Добавил домашнее задание

---
+++ Проверка на чётность/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Program
+{
+    public class HomeWorks
+    {
+        static void Main(string[] args)
+        {
+            //Задача такая: Нужно определить число на чётность и нечётность.
+
+            Console.WriteLine("Введите число");
+            int value = Convert.ToInt32(Console.ReadLine());
+            if (value % 2 == 0)
+            {
+                Console.WriteLine("Число чётное!");
+            }
+            else
+            {
+                Console.WriteLine("Число нечётное!");
+            }
+        }
+    }
+}
