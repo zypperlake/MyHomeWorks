3e2baf9c1379e0d73dc3711bbae77f8f197dbc79
Sobirjon Matnazarov <matnazarovsobirjon123@outlook.com>
2022-11-26 22:39:20 +0500

    Правки по проекту

---
+++ Сумма и произведения трёх чисел/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Program
+{
+    public class HomeWorks
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 5;
+            int c = 6;
+
+            Console.WriteLine("Сумма и произведения трёх чисел");
+            Console.WriteLine("===============================");
+            Console.WriteLine($"Заданы: a = {a}, b = {b}, c = {c}");
+            Console.WriteLine("Сумма => " + (a + b + c));
+            Console.WriteLine("Произведения => " + (a - b - c));
+        }
+    }
+}
