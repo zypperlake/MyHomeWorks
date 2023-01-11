9daec76047962a536dd354b1936921090769bae6
Собиржон Матназаров <s.matnazarov@prodmag.com>
2023-01-11 13:33:53 +0500

    Дополнил домашним заданием с массивов.

---
+++ Вввод массива с клавиатуры/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Вввод_массива_с_клавиатуры
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========================");
+            Console.Write("Введите диапазон массива: ");
+            int input_count = 0;
+            try { input_count = int.Parse(Console.ReadLine()); }
+            catch { Console.WriteLine("Введено неккоректное число!"); }
+
+            int[] Massive = new int[input_count];
+
+            Console.WriteLine("=========================");
+
+            for(int i = 0; i < Massive.Length; )
+            {
+                Console.WriteLine($"Введите значение элемента с индексом {i}:");
+                try { Massive[i] = int.Parse(Console.ReadLine()); i++; }
+                catch {
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("Не удалось добавить элемент! \nПопробуйте ещё раз!");
+                    Console.WriteLine("=========================");
+                }
+            }
+
+            Console.WriteLine("=========================");
+            Console.WriteLine("Вывод массива:");
+
+            for (int i = 0; i < Massive.Length; i++)
+            {
+                Console.WriteLine(Massive[i]);
+            }
+        }
+    }
+}
