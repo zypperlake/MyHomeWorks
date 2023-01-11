28d28b2bb49fa8b87d3db2d3ab13ef854c07fcbd
Собиржон Матназаров <s.matnazarov@prodmag.com>
2023-01-11 14:47:45 +0500

    Добавил задание вывода массива в обратном порядке.

---
+++ Вывод массива в обратном порядке/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("====================");
+            //Вывод по первому варианту
+            for (int count = ints.Length - 1; count >= 0; count--)
+            {
+                Console.Write("{0} {1}", ints[count], "");
+            }
+
+            Console.WriteLine("\n====================");
+
+            //Вывод по второму варианту
+            for (int count = ints.Length; count > 0;)
+            {
+                --count;
+                Console.Write("{0} {1}", ints[count], "");
+            }
+            Console.WriteLine("\n====================");
+        }
+    }
+}
--- Типы данных/Program.cs
+++ Типы данных/Program.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Program
+﻿namespace Program
 {
     public class Program
     {
